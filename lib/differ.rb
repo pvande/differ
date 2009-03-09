5ef3cfc50a919457d85bf1ea8ea67856cb48a4ab
pvande <pvande@gmail.com>
2009-03-08 23:35:10 -0700

    Cleanup work

--- lib/differ.rb
+++ lib/differ.rb
@@ -21,36 +21,48 @@ module Differ
   class << self
     include Helpers
 
-  private
-    def change(method, array, index)
-      send(method, *array.slice!(0..index))
-      same(array.shift)
+    def diff(target, source, separator)
+      old_sep, $; = $;, separator
+      @results = []
+
+      target = target.split(separator)
+      source = source.split(separator)
+
+      advance(target, source) until source.empty? || target.empty?
+      insert(*target) || delete(*source)
+
+      $; = old_sep
+      @results
     end
 
     def diff_by_char(to, from)
-      @results = []
-      a = { :from => from.split(''), :to => to.split('') }
+      diff(to, from, '')
+    end
 
-      until a[:from].empty? || a[:to].empty?
-        from, to = a.values_at(:from, :to).collect { |el| el.shift }
-        same(to) && next if from == to
+  private
+    def advance(target, source)
+      del, add = source.shift, target.shift
 
-        prioritize_insert = a[:to].length > a[:from].length
-        if (insert_index = a[:to].index(from)) && prioritize_insert
-          change(:insert, a[:to].unshift(to), insert_index)
-        elsif (delete_index = a[:from].index(to))
-          change(:delete, a[:from].unshift(from), delete_index)
-        elsif insert_index
-          change(:insert, a[:to].unshift(to), insert_index)
-        else
-          insert(to) && delete(from)
-        end
+      prioritize_insert = target.length > source.length
+      insert = target.index(del)
+      delete = source.index(add)
+
+      if del == add
+        same(add)
+      elsif insert && prioritize_insert
+        change(:insert, target.unshift(add), insert)
+      elsif delete
+        change(:delete, source.unshift(del), delete)
+      elsif insert && !prioritize_insert
+        change(:insert, target.unshift(add), insert)
+      else
+        insert(add) && delete(del)
       end
+    end
 
-      insert(*a[:to])   unless a[:to].empty?
-      delete(*a[:from]) unless a[:from].empty?
-
-      @results
+    def change(method, array, index)
+      send(method, *array.slice!(0..index))
+      same(array.shift)
     end
   end
 end
--- lib/differ/helpers.rb
+++ lib/differ/helpers.rb
@@ -1,17 +1,20 @@
 module Differ
   module Helpers
     def same(*str)
+      return if str.empty?
       last = @results.last
       (last.is_a?(String) ? last : @results) << str.join
     end
 
     def delete(*str)
+      return if str.empty?
       last = @results.last
       @results.push(last = Change.new) unless last.is_a?(Change)
       last.del << str.join
     end
 
     def insert(*str)
+      return if str.empty?
       last = @results.last
       @results.push(last = Change.new) unless last.is_a?(Change)
       last.add << str.join
